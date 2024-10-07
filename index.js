beb56c3e4ed7eab4653be4543e79e0eed4bd323a
IulikTKW <inicula1989uk@gmail.com>
2024-10-07 20:49:20 +0300

    1

---
+++ index.js
@@ -0,0 +1,111 @@
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// let i = 20;
+
+// while (--i) {
+//   console.log(i)
+// }
+
+// do {
+//     console.log(i)
+//     i++
+// } while (i < 10)
+
+
+// for (let i = 10; i >= 1 ;i--) {
+//     console.log(i)
+// }
+
+// function greetings(name = 'User') {
+//     console.log(arguments)
+//     console.log(`Hi, my name is: ${name}`)
+  
+// }
+
+// greetings('Alex')
+// greetings('Vasile', 'Victor', 'Alex')
+
+// const sum = function(num1, num2) {
+//     console.log(arguments)
+//     console.log(num1 + num2)
+// }
+
+//sum(2, 3)
+
+//const addTwo = (num) =>  console.log(num + 2)
+
+// addTwo(6)
+
+//1: sa inmulteasca cu 2
+
+// const multiplyByTwo = (num) => {
+//     if(typeof num !== 'number' || isNaN(num)) return 'please use a number'    
+
+//     return num * 2;    
+// }
+
+// const firstResult = multiplyByTwo(4)
+
+//console.log(firstResult)
+//2: intoare numele comple, primeste 2 argumente nume/prenume
+
+// const showFullName = (firstName, lastName) => {
+//     if(typeof firstName === 'string' && typeof lastName === 'string') {
+//         return `Full name is: ${firstName} ${lastName}`
+//     }
+
+//     return 'please use a valid name'
+    
+// }
+
+// const fullName = showFullName('Iulian', 30)
+
+// console.log(fullName)
+//3: welcome back: argument guest;
+
+// function welcomeFunc(name = 'guest') {
+//     console.log('Welcome back: ' + name)
+// }
+//4: scade 2 1 argument 
+
+// welcomeFunc('Alex')
+// welcomeFunc()
+
+// const minusTwo = function(num) {
+//     console.log(num - 2)
+// }
+
+// minusTwo(4)
+
+// var x ;
+
+// console.log(x)
+
+// var x = 5;
+
+
+// console.log(x)
+
+
+// console.log(z)
+
+// let y = 5
+// const z = 6
+
+
+// sayHi()
+
+
+// const sayHi = () => {
+//     console.log('Hi')
+// }
+
+
+
+
+
